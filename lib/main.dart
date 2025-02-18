0dd51ff9a3cd9964b0852e808d8748acdb47b3ab
afiqq03 <afiqd503@gmail.com>
2025-02-18 11:48:47 +0800

    add information

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: IdCard(),
-));
+      home: IdCard(),
+    ));
 
 class IdCard extends StatelessWidget {
   const IdCard({super.key});
@@ -10,37 +10,85 @@ class IdCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: const Text('ID Card', 
-        style: TextStyle(color: Colors.white),),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          children: <Widget>[
-            const Text(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: const Text(
+            'ID Card',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/psp.png'),
+                  radius: 60.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[800],
+              ),
+              const SizedBox(height: 10.0),
+            const Text( 
               'NAME',
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
               ),
             ),
+            const SizedBox(height: 10.0),
             Text(
-              'Fiqq',
+              'username',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 fontSize: 28.0,
                 fontWeight: FontWeight.bold,
                 letterSpacing: 2.0,
               ),
-            )
-          ]
-        ),
-      )
-    );
+            ),
+            const SizedBox(height: 10.0),
+            const Text(
+              'Nationality',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Malaysia',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'email@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0, 
+                  ),
+                )
+              ],
+            ),
+          ]),
+        ));
   }
 }
